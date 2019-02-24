56849cab3afa359e7350773279701b984c6308d0
Anawat Muangjai <anawat.muangjai@gmail.com>
2019-02-24 15:57:35 +0700

    Add unit test and example app

---
+++ src/Dijkstra.Client/Program.cs
@@ -0,0 +1,66 @@
+﻿using Dijkstra.Core.Engine;
+using Dijkstra.Core.Graph;
+using System;
+
+namespace Dijkstra.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindShortestPath();
+        }
+
+        static void FindShortestPath()
+        {
+            // Create node
+            var nodeA = new Node<string>("Chiang Mai");
+            var nodeB = new Node<string>("Lampang");
+            var nodeC = new Node<string>("Sukhothai");
+            var nodeD = new Node<string>("Ayuthaya");
+            var nodeE = new Node<string>("Bangkok");
+
+            // Create graph data
+            var graph = new Graph<string>();
+
+            // Add node
+            graph.AddNode(nodeA);
+            graph.AddNode(nodeB);
+            graph.AddNode(nodeC);
+            graph.AddNode(nodeD);
+            graph.AddNode(nodeE);
+
+            // Add edges
+            graph.AddEdge(nodeA, nodeB, 3);
+            graph.AddEdge(nodeA, nodeC, 5);
+            graph.AddEdge(nodeB, nodeA, 3);
+            graph.AddEdge(nodeB, nodeC, 1);
+            graph.AddEdge(nodeB, nodeD, 2);
+            graph.AddEdge(nodeC, nodeA, 5);
+            graph.AddEdge(nodeC, nodeB, 1);
+            graph.AddEdge(nodeC, nodeD, 3);
+            graph.AddEdge(nodeC, nodeE, 6);
+            graph.AddEdge(nodeD, nodeB, 2);
+            graph.AddEdge(nodeD, nodeC, 3);
+            graph.AddEdge(nodeD, nodeE, 4);
+            graph.AddEdge(nodeE, nodeC, 6);
+            graph.AddEdge(nodeE, nodeD, 4);
+
+            // Create path engine
+            var pathEngine = new PathEngine<string>(graph);
+            var pathResult = pathEngine.FindShortestPath(nodeA, nodeE);
+
+            // Display path result
+            Console.WriteLine($"Total cost from {pathResult.From} to {pathResult.To} is {pathResult.TotalCost}");
+            Console.WriteLine("============================================");
+
+            foreach (var path in pathResult.Paths)
+            {
+                Console.WriteLine(path.ToString());
+            }
+
+            Console.WriteLine("============================================");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tests/Dijkstra.Tests/Engine/PathEngineTests.cs
@@ -0,0 +1,95 @@
+using Dijkstra.Core.Engine;
+using Dijkstra.Core.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dijkstra.Tests.Engine
+{
+    [TestClass]
+    public class PathEngineTests
+    {
+        private PathEngine<string> _pathEngine;
+        private Node<string> _nodeA;
+        private Node<string> _nodeB;
+        private Node<string> _nodeC;
+        private Node<string> _nodeD;
+        private Node<string> _nodeE;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _nodeA = new Node<string>("Chiang Mai");
+            _nodeB = new Node<string>("Lampang");
+            _nodeC = new Node<string>("Sukhothai");
+            _nodeD = new Node<string>("Ayuthaya");
+            _nodeE = new Node<string>("Bangkok");
+
+            var graph = new Graph<string>();
+
+            // Add node
+            graph.AddNode(_nodeA);
+            graph.AddNode(_nodeB);
+            graph.AddNode(_nodeC);
+            graph.AddNode(_nodeD);
+            graph.AddNode(_nodeE);
+
+            // Add edges
+            graph.AddEdge(_nodeA, _nodeB, 3);
+            graph.AddEdge(_nodeA, _nodeC, 5);
+            graph.AddEdge(_nodeB, _nodeA, 3);
+            graph.AddEdge(_nodeB, _nodeC, 1);
+            graph.AddEdge(_nodeB, _nodeD, 2);
+            graph.AddEdge(_nodeC, _nodeA, 5);
+            graph.AddEdge(_nodeC, _nodeB, 1);
+            graph.AddEdge(_nodeC, _nodeD, 3);
+            graph.AddEdge(_nodeC, _nodeE, 6);
+            graph.AddEdge(_nodeD, _nodeB, 2);
+            graph.AddEdge(_nodeD, _nodeC, 3);
+            graph.AddEdge(_nodeD, _nodeE, 4);
+            graph.AddEdge(_nodeE, _nodeC, 6);
+            graph.AddEdge(_nodeE, _nodeD, 4);
+
+            _pathEngine = new PathEngine<string>(graph);
+        }
+
+        [TestMethod]
+        public void FindShortestPath_FromNodeAToNodeE_TotalCostShould9()
+        {
+            var result = _pathEngine.FindShortestPath(_nodeA, _nodeE);
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(9, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void FindShortestPath_FromNodeAToNodeD_TotalCostShould5()
+        {
+            var result = _pathEngine.FindShortestPath(_nodeA, _nodeD);
+
+            Assert.AreEqual(5, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void FindShortestPath_FromNodeAToNodeC_TotalCostShould4()
+        {
+            var result = _pathEngine.FindShortestPath(_nodeA, _nodeC);
+
+            Assert.AreEqual(4, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void FindShortestPath_FromNodeEToNodeA_TotalCostShould9()
+        {
+            var result = _pathEngine.FindShortestPath(_nodeE, _nodeA);
+
+            Assert.AreEqual(9, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void FindShortestPath_FromNodeBToNodeE_TotalCostShould9()
+        {
+            var result = _pathEngine.FindShortestPath(_nodeE, _nodeA);
+
+            Assert.AreEqual(9, result.TotalCost);
+        }
+    }
+}
