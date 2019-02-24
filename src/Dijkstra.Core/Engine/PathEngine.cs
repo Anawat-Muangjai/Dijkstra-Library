c825455dc7915c35e3f94e7296e84a88bc34e725
Anawat Muangjai <anawat.muangjai@gmail.com>
2019-02-24 15:56:06 +0700

    Add dijkstra calculation class

---
+++ src/Dijkstra.Core/Engine/PathEngine.cs
@@ -0,0 +1,78 @@
+﻿using Dijkstra.Core.Graph;
+using Priority_Queue;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra.Core.Engine
+{
+    public class PathEngine<T>
+    {
+        private readonly Graph<T> _graph;
+
+        public PathEngine(Graph<T> graph)
+        {
+            _graph = graph;
+        }
+
+        public PathResult<T> FindShortestPath(Node<T> source, Node<T> destination)
+        {
+            var pathResult = new PathResult<T>();
+            var totalCost = new Dictionary<Node<T>, int>();
+            var previousNodes = new Dictionary<Node<T>, Edge<T>>();
+            var priorityQueue = new SimplePriorityQueue<Node<T>>();
+
+            foreach (var node in _graph.Nodes)
+            {
+                if (node.Value.Equals(source.Value))
+                    totalCost[node] = 0;
+                else
+                    totalCost[node] = int.MaxValue;
+
+                priorityQueue.Enqueue(node, totalCost[node]);
+            }
+
+            while (priorityQueue.Count != 0)
+            {
+                var minNode = priorityQueue.Dequeue();
+
+                if (minNode.Value.Equals(destination.Value))
+                {
+                    while (previousNodes.ContainsKey(minNode))
+                    {
+                        pathResult.Insert(previousNodes[minNode], 0);
+                        minNode = pathResult.From;
+                    }
+
+                    break;
+                }
+
+                var neighborEdges = _graph.Edges.FindAll(x => x.From.Equals(minNode));
+
+                foreach (var neighbor in neighborEdges)
+                {
+                    Node<T> neighborNode;
+
+                    if (neighbor.From.Equals(minNode))
+                        neighborNode = neighbor.To;
+                    else
+                        neighborNode = neighbor.From;
+
+                    var smallestCost = totalCost[minNode];
+                    var neighborCost = neighbor.Cost;
+                    var sumCost = smallestCost + neighborCost;
+
+                    if (sumCost < totalCost[neighborNode])
+                    {
+                        totalCost[neighborNode] = sumCost;
+                        previousNodes[neighborNode] = neighbor;
+
+                        priorityQueue.UpdatePriority(neighborNode, sumCost);
+                    }
+                }
+            }
+
+            return pathResult;
+        }
+    }
+}
---
+++ src/Dijkstra.Core/Engine/PathResult.cs
@@ -0,0 +1,60 @@
+﻿using Dijkstra.Core.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dijkstra.Core.Engine
+{
+    public class PathResult<T>
+    {
+        public List<Edge<T>> Paths { get; private set; }
+
+        public int Count
+        {
+            get
+            {
+                return Paths.Count();
+            }
+        }
+
+        public int TotalCost
+        {
+            get
+            {
+                return Paths.Sum(x => x.Cost);
+            }
+        }
+
+        public Node<T> From
+        {
+            get
+            {
+                return Paths[0].From;
+            }
+        }
+
+        public Node<T> To
+        {
+            get
+            {
+                return Paths[Count - 1].To;
+            }
+        }
+
+        public PathResult(List<Edge<T>> edges = null)
+        {
+            Paths = edges ?? new List<Edge<T>>();
+        }
+
+        public void Insert(Edge<T> edge, int index)
+        {
+            Paths.Insert(index, edge);
+        }
+
+        public override string ToString()
+        {
+            return $"From: {From} To: {To} Cost: {TotalCost}";
+        }
+    }
+}
